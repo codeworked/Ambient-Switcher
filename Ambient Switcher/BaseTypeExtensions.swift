8bba100242004f8842652bcfabb8e8fe23112709
Alex Geleta <codeworked@outlook.com>
2019-11-14 01:13:48 +0300

    Base type extensions moved to BaseTypeExtension file.

--- Ambient Switcher/BaseTypeExtensions.swift
+++ Ambient Switcher/BaseTypeExtensions.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2019 Александр Гелета. All rights reserved.
 //
 
+import Cocoa
 import Foundation
 import OSLog
 
@@ -40,3 +41,24 @@ extension Int {
         return self > min && self < max
     }
 }
+
+extension String {
+    /// Takes localized string for current language from
+    /// project localized strings files.
+    ///
+    /// Usage is very lean and simple:
+    ///
+    /// ```
+    ///     "yourLocalizedString".localized()
+    /// ```
+    ///
+    /// - Parameter bundle: Current application bundle. By default it is set to .main.
+    /// - Parameter tableName: Collection of localized string. By default it is set to "Main".
+    ///
+    /// - Returns: Returns NSLocalizedString from localized collection. If localized string didn't
+    /// found then will be returned it's name.
+    func localized(bundle: Bundle = .main, tableName: String = "Main") -> String {
+        return NSLocalizedString(self, tableName: tableName, value: "**\(self)**", comment: "")
+    }
+}
+
